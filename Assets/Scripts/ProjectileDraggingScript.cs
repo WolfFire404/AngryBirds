ced3cf222832c58c5f2f314dc4a6a11e9cfd235d
Elgianni <22642@ma-web.nl>
2018-01-15 11:54:26 +0100

    Misschien nu wel

---
+++ Assets/Scripts/ProjectileDraggingScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileDraggingScript : MonoBehaviour {
+
+    public float maxStretch = 3.0f;
+    //public LineRenderer Katapult_achterkant;
+    //public LineRenderer Katapult_voorkant;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnMouseDown()
+    {
+        
+    }
+}
--- Assets/Scripts/TileScript.cs
+++ Assets/Scripts/TileScript.cs
@@ -4,13 +4,39 @@ using UnityEngine;
 
 public class TileScript : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    public float TileHealth = 300;
+
+    // Use this for initialization
+    void Start () {
+        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.relativeVelocity.magnitude < 7 && collision.relativeVelocity.magnitude > 4)
+        {
+            TileHealth -= 50f;
+        }
+
+        else if (collision.relativeVelocity.magnitude < 10 && collision.relativeVelocity.magnitude > 7)
+        {
+           TileHealth -=200f;
+        }
+
+        else if (collision.relativeVelocity.magnitude > 10)
+        {
+           TileHealth -=300f;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(TileHealth);
+        if(TileHealth < 1f)
+        {
+            Destroy(gameObject);
+        }
+    }
 }
