3810eefdcf2b0a1d9f8a40375c8f73294fa3624d
Elgianni <22642@ma-web.nl>
2018-01-18 11:21:08 +0100

    updated catapult

--- Assets/Scripts/ProjectileDraggingScript.cs
+++ Assets/Scripts/ProjectileDraggingScript.cs
@@ -2,24 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ProjectileDraggingScript : MonoBehaviour {
+public class ProjectileDraggingScript : MonoBehaviour
+{
 
-    public float maxStretch = 3.0f;
+    private Vector2 screenPoint;
+    private Vector2 offset;
+
+    /*public float maxStretch = 3.0f;
     //public LineRenderer Katapult_achterkant;
     //public LineRenderer Katapult_voorkant;
 
+    private SpringJoint2D spring;
+
+    private void Awake()
+    {
+        spring = GetComponent<SpringJoint2D>();
+    }
+
     // Use this for initialization
     void Start () {
-		
-	}
+        //joint = GetComponent<HingeJoint>();
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        //joint.breakForce = 10;
+    }*/
 
-    private void OnMouseDown()
+    void OnMouseDown()
     {
-        
+        Debug.Log("I'm down");
+        Vector2 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = (pos);
     }
+
 }
--- Assets/Scripts/TileScript.cs
+++ Assets/Scripts/TileScript.cs
@@ -5,35 +5,36 @@ using UnityEngine;
 public class TileScript : MonoBehaviour {
 
 
-    public float TileHealth = 300;
+    public float TileHealth = 3f;
 
     // Use this for initialization
     void Start () {
         
     }
 
-    void OnCollisionEnter(Collision collision)
+    void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.relativeVelocity.magnitude < 7 && collision.relativeVelocity.magnitude > 4)
+        if (collision.relativeVelocity.magnitude < 5 && collision.relativeVelocity.magnitude > 3)
         {
-            TileHealth -= 50f;
+            TileHealth -=1f;
         }
 
-        else if (collision.relativeVelocity.magnitude < 10 && collision.relativeVelocity.magnitude > 7)
+        else if (collision.relativeVelocity.magnitude < 7 && collision.relativeVelocity.magnitude > 5)
         {
-           TileHealth -=200f;
+           TileHealth -=2f;
         }
 
-        else if (collision.relativeVelocity.magnitude > 10)
+        else if (collision.relativeVelocity.magnitude > 7)
         {
-           TileHealth -=300f;
+           TileHealth -=3f;
         }
+        Debug.Log(collision.relativeVelocity.magnitude);
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(TileHealth);
+        //Debug.Log(TileHealth);
         if(TileHealth < 1f)
         {
             Destroy(gameObject);
