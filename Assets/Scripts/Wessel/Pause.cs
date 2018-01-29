4ece78baf64da3fd352faafa959bd6c9805c3fdf
wesselvink <22092@ma-web.nl>
2018-01-29 09:51:25 +0100

    blokken

---
+++ Assets/Scripts/Wessel/Pause.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Pause : MonoBehaviour {
+	public Transform pause_Canvas;
+	public Transform Buttons;
+		public Transform Player;
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+			PauseGame ();
+		}
+	}
+
+	public void Back() {
+		Buttons.gameObject.SetActive (true);
+	}
+
+
+	public void PauseGame() {
+		if (pause_Canvas.gameObject.activeInHierarchy == false) {
+			pause_Canvas.gameObject.SetActive (true);
+			Time.timeScale = 0;
+
+		} else {
+			pause_Canvas.gameObject.SetActive (false);
+			Time.timeScale = 1;
+	
+		}
+	}
+	}
