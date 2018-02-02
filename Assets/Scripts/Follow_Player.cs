0c91ca6203b4b3010601dd9707816e2686f462bb
Elgianni <22642@ma-web.nl>
2018-02-02 15:04:29 +0100

    added Wezzel's zijn scripts

--- Assets/Scripts/Follow_Player.cs
+++ Assets/Scripts/Follow_Player.cs
@@ -6,6 +6,7 @@ public class Follow_Player : MonoBehaviour {
     public GameObject Sphere;     //Public variable to store a reference to the player game object
 
 	private Vector3 offset;         //Private variable to store the offset distance between the player and camera
+    private float _camPosition;
 
 
     ProjectileDraggingScript projectiledraggingscript;
@@ -31,12 +32,14 @@ public class Follow_Player : MonoBehaviour {
 	// LateUpdate is called after Update each frame
 	void Update () 
 	{
+        _camPosition = Sphere.transform.position.x;
+
             //Calculate and store the offset value by getting the distance between the player's position and camera's position.
-        if(projectiledraggingscript.BeenShot)
+        if (projectiledraggingscript.BeenShot)
         {
-           // Debug.Log("Oh Fuck Off");
-            transform.position = Sphere.transform.position + offset;
+            // Debug.Log("Oh Fuck Off"); //mijn reactie toen het toch wel werkte
+            transform.position = new Vector3(_camPosition, 0) + offset;
         }
-            
+
     }
 }
